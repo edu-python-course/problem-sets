149ad70fb9aa90f6e28e2dd4c04cc9333b448646
Serhii Horodilov <sgorodil@gmail.com>
2022-11-26 02:29:06 +0200

    Updated convenience store package test cases
    Avoid True comparing while trying to append the same product to
    the shopping cart (newly created products are considered the same,
    if have the same name and price as existing products).
    Added __eq__ test cases for the product model.

--- tests/conv_store/test_models.py
+++ tests/conv_store/test_models.py
@@ -10,12 +10,20 @@ class TestProductModel(unittest.TestCase):
         self.product_price = 15.1
         self.instance = models.Product(self.product_name, self.product_price)
 
-    def test_constructor(self):
+    def test_initializer(self):
         self.assertEqual(self.product_name, self.instance.name)
         self.assertEqual(self.product_price, self.instance.price)
 
     def test_str(self):
         self.assertEqual(self.product_name, str(self.instance))
+
+    def test_equality_comparing(self):
+        another = models.Product("apple", 15.1)
+        self.assertEqual(another, self.instance)
+
+    def test_equality_raises(self):
+        self.assertRaises(TypeError, self.instance.__eq__, "apple")
+        self.assertRaises(TypeError, self.instance.__eq__, 15.1)
 
     def test_get_total(self):
         self.assertEqual(10.57, self.instance.get_total(0.7))
@@ -42,11 +50,13 @@ class TestShoppingCartModel(unittest.TestCase):
         self.assertListEqual(self.quantities + [.1], self.instance.quantities)
 
     def test_add_existing_product(self):
-        apple, juice = self.products
+        # avoid existing objects usage
+        apple = models.Product("apple", 10.59)
+        juice = models.Product("juice", 36.55)
+        self.quantities = [self.quantities[0] + 1.5, self.quantities[1] + 2]
+
         self.instance.add_product(apple, 1.5)
         self.instance.add_product(juice, 2)
-        self.quantities[0] += 1.5
-        self.quantities[1] += 2
         self.assertListEqual(self.products, self.instance.products)
         self.assertListEqual(self.quantities, self.instance.quantities)
 
