3fb0fb8033e6885ee6bbb7e93efda931397618f1
Serhii Horodilov <sgorodil@gmail.com>
2022-11-30 02:57:55 +0200

    Updated conv store models
    Updated models implementation and test cases according to challenge description.

--- tests/conv_store/test_models.py
+++ tests/conv_store/test_models.py
@@ -6,59 +6,121 @@ from conv_store import models
 class TestProductModel(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.product_name = "apple"
-        self.product_price = 15.1
-        self.instance = models.Product(self.product_name, self.product_price)
+        self.product_name = "candy"
+        self.product_price = 10.59
+        self.product_unit = 0.1
+        self.instance = models.Product(
+            self.product_name, self.product_price, self.product_unit
+        )
 
     def test_initializer(self):
         self.assertEqual(self.product_name, self.instance.name)
         self.assertEqual(self.product_price, self.instance.price)
+        self.assertEqual(self.product_unit, self.instance.unit)
 
     def test_str(self):
         self.assertEqual(self.product_name, str(self.instance))
 
-    def test_equality_comparing(self):
-        another = models.Product("apple", 15.1)
-        self.assertEqual(another, self.instance)
+    def test_repr(self):
+        values = self.product_name, self.product_price, self.product_unit
+        test_value = "Product('%s', %.2f, %s)" % values
+        self.assertEqual(repr(self.instance), test_value)
 
-    def test_equality_raises(self):
-        self.assertRaises(TypeError, self.instance.__eq__, "apple")
-        self.assertRaises(TypeError, self.instance.__eq__, 15.1)
+    def test_float(self):
+        self.assertEqual(float(self.instance), self.product_price)
+
+    def test_equal(self):
+        another = models.Product("candy", 10.59, 0.1)
+        self.assertEqual(self.instance, another)
+
+    def test_not_equal(self):
+        another = models.Product("juice", 36.55, 1)
+        self.assertNotEqual(self.instance, another)
+        another = models.Product("candy", 36.55, 1)
+        self.assertNotEqual(self.instance, another)
+        another = models.Product("candy", 10.59, 1)
+        self.assertNotEqual(self.instance, another)
+        self.assertNotEqual(self.instance, "candy")
+        self.assertNotEqual(self.instance, 10.59)
 
     def test_get_total(self):
-        self.assertEqual(10.57, self.instance.get_total(0.7))
+        self.assertEqual(74.13, self.instance.get_total(0.7))
 
 
 class TestShoppingCartModel(unittest.TestCase):
     def setUp(self) -> None:
         self.instance = models.ShoppingCart()
-        apple = models.Product("apple", 10.59)
-        juice = models.Product("juice", 36.55)
-        self.products = [apple, juice]
+        candy = models.Product("candy", 10.59, 0.1)
+        juice = models.Product("juice", 36.55, 1)
+        self.products = [candy, juice]
         self.quantities = [0.7, 4]
-        self.instance.add_product(apple, 0.7)
+        self.instance.add_product(candy, 0.7)
         self.instance.add_product(juice, 4)
 
     def test_products(self):
         self.assertListEqual(self.products, self.instance.products)
         self.assertListEqual(self.quantities, self.instance.quantities)
 
+    @unittest.skip(reason="Not implemented")
+    def test_remove_product(self):
+        candy = models.Product("candy", 10.59, 0.1)
+        self.instance.remove_product(candy)
+        self.assertNotIn(candy, self.instance.products)
+        self.assertEqual(len(self.instance.products), 1)
+        self.assertEqual(len(self.instance.quantities), 1)
+
     def test_add_product(self):
-        spice = models.Product("spice", 120.25)
-        self.instance.add_product(spice, .100)
+        spice = models.Product("spice", 120.25, 0.1)
+        self.instance.add_product(spice)
         self.assertListEqual(self.products + [spice], self.instance.products)
         self.assertListEqual(self.quantities + [.1], self.instance.quantities)
 
     def test_add_existing_product(self):
         # avoid existing objects usage
-        apple = models.Product("apple", 10.59)
-        juice = models.Product("juice", 36.55)
-        self.quantities = [self.quantities[0] + 1.5, self.quantities[1] + 2]
+        candy = models.Product("candy", 10.59, 0.1)
+        juice = models.Product("juice", 36.55, 1)
+        quantities = [self.quantities[0] + 1.5, self.quantities[1] + 2]
 
-        self.instance.add_product(apple, 1.5)
+        self.instance.add_product(candy, 1.5)
         self.instance.add_product(juice, 2)
         self.assertListEqual(self.products, self.instance.products)
-        self.assertListEqual(self.quantities, self.instance.quantities)
+        self.assertListEqual(quantities, self.instance.quantities)
+
+    @unittest.skip(reason="Not implemented")
+    def test_sub_product(self):
+        juice = models.Product("juice", 36.55, 1)
+        self.instance.sub_product(juice, 2)
+        self.assertEqual(self.instance.quantities[1], 3)
+
+    @unittest.skip(reason="Not implemented")
+    def test_sub_product_removes(self):
+        juice = models.Product("juice", 36.55, 1)
+        self.instance.sub_product(juice, 5)
+        self.assertNotIn(juice, self.instance.products)
+        self.assertEqual(len(self.instance.products), 1)
+        self.assertEqual(len(self.instance.quantities), 1)
 
     def test_get_total(self):
-        self.assertEqual(153.61, self.instance.get_total())
+        self.assertEqual(220.33, self.instance.get_total())
+
+    def test_empty(self):
+        self.assertTrue(self.instance)
+        self.assertFalse(models.ShoppingCart())
+
+    def test_len(self):
+        self.assertEqual(len(self.instance), 2)
+
+    def test_get_item(self):
+        test_value = models.Product("juice", 36.55, 1), 4
+        self.assertTupleEqual(self.instance[1], test_value)
+
+    def test_contains(self):
+        juice = models.Product("juice", 36.55, 1)
+        eggs = models.Product("eggs", 65.0, 10)
+        self.assertIn(juice, self.instance)
+        self.assertNotIn(eggs, self.instance)
+
+    def test_iteration(self):
+        test_values = zip(self.products, self.quantities)
+        for test_value, cart_item in zip(test_values, self.instance):
+            self.assertTupleEqual(test_value, cart_item)
