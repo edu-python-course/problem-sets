e9864f94edce7d6c664b6e55b0ec5be7f0b6529d
Serhii Horodilov <sgorodil@gmail.com>
2023-01-14 12:25:23 +0200

    Dynamic programming (#17)
    * Rebased 'feature/dynamic' onto 'devel'
    
    * Added factorial function
    
    Added factorial getter function implementation and tests.
    Added typed marker file for the "calc" package.
    
    * Added dynamic package
    
    Implemented fibonacci number getter function (dynamic programming).
    Added corresponding test cases for huge numbers to demonstrate enhancements.
    
    * Added grid traveler problem
    
    * Updated function signatures
    
    * Updated memoize decorator documentation
    
    * Updated imports
    
    * Fixed pylint issues
    
    * Added can sum target function
    
    Implemented function to check, if it's available to get the target number.
    Added test cases as well.
    Only tabulation approach implemented.
    
    * Updated memoize decorator docstring
    
    * Added can sum target function
    
    Implemented memoized function and test cases.
    
    * Refactored "can_get_target" function
    
    * Updated tabulation functions
    
    Implemented function to get the best sequence to get target number.
    Added test cases as well.
    This algorithm has no limitations on available numbers usage.
    
    * Added some comments to dynamic functions
    
    * Added partition list function boilerplate
    
    Prepared some test cases, including edge cases tests.
    TDD - test are expected failed (no pytest marks).
    
    * Revert "Added partition list function boilerplate"
    
    This reverts commit 8a49e9be72743d5bd27fcb9a297311561b44a0b1.

---
+++ src/dynamic/memoization.py
@@ -0,0 +1,115 @@
+"""
+Dynamic programming memoization functions
+
+"""
+
+import functools
+from typing import Any, Callable, Dict, List, Optional, Tuple
+
+from calc.func import get_fibonacci_number as fib_classic
+
+
+def memoize(func: Callable) -> Callable:
+    """Memoization decorator implementation
+
+    :param func: a function to decorate with memoization technique
+
+    :return: the decorated function
+
+    The memoization technique helps to reduce the number of recursive
+    calls by caching the results already calculated for some inputs.
+
+    Current implementation caches the inputs as tuples containing args,
+    so it has limitations for "un-hashable" inputs, that can not be
+    stored in a cache dictionary.
+
+    """
+
+    cache: Dict[Tuple[Any], int] = {}
+
+    @functools.wraps(func)
+    def wrapper(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+
+        return cache[args]
+
+    return wrapper
+
+
+@memoize
+@functools.wraps(fib_classic, ("__annotations__", "__doc__"))
+def get_fibonacci_number(idx):  # pylint: disable=C0116
+    if idx <= 0:
+        return 0
+    if idx < 3:
+        return 1
+
+    return get_fibonacci_number(idx - 1) + get_fibonacci_number(idx - 2)
+
+
+@memoize
+def get_grid_travels(height: int, width: int, /) -> int:
+    """Calculate the number of available route for a specified grid size
+
+    :param height: grid height
+    :param width: grid width
+
+    :return: the number of available routes
+    :rtype: int
+
+    The traveler starts the journey in the top-left corner of the grid
+    and trying to reach the opposite grid corner (bottom-right).
+    The only moves available are **move right** and **move down**.
+    The task is to calculate the number of all available routes to do
+    this.
+
+    """
+
+    if height <= 0 or width <= 0:
+        return 0
+
+    if height == 1 or width == 1:
+        return 1
+
+    return (get_grid_travels(height - 1, width)
+            + get_grid_travels(height, width - 1))
+
+
+def can_get_target(target: int, numbers: List[int],
+                   cache: Optional[Dict[int, bool]] = None) -> bool:
+    """Check if the target value can be generated using given numbers
+
+    :param target: the desired number
+    :type target: int
+    :param numbers: the sequence of numbers available for usage
+    :type numbers: list[int]
+    :param cache: optional dictionary that stores already calculated results
+    :type cache: dict[int, bool]
+
+    :return: the check result
+    :rtype: bool
+
+    Numbers from the list can be used as many times as needed.
+
+    """
+
+    # check base cases
+    if target < 0:
+        return False
+
+    if not target:
+        return True
+
+    # check cached values
+    cache = cache or {}
+    if target in cache:
+        return cache[target]
+
+    # perform calculation
+    for number in numbers:
+        cache[target] = can_get_target(target - number, numbers, cache)
+        if cache[target]:
+            return True
+
+    return False
--- tests/sequences/test_func.py
+++ tests/sequences/test_func.py
@@ -55,15 +55,6 @@ def test_parentheses_validator():
     assert sequences.are_parentheses_balanced("(some <text>)") is True
 
 
-def test_fibonacci_number_getter():
-    assert sequences.get_fibonacci_number(-10) == 0  # special test case
-    assert sequences.get_fibonacci_number(0) == 0
-    assert sequences.get_fibonacci_number(1) == 1
-    assert sequences.get_fibonacci_number(2) == 1
-    assert sequences.get_fibonacci_number(3) == 2
-    assert sequences.get_fibonacci_number(9) == 34
-
-
 # noinspection SpellCheckingInspection
 def test_get_longest_uniq_sequence_length():
     assert sequences.get_longest_uniq_length("abcdefg") == 7
@@ -71,27 +62,4 @@ def test_get_longest_uniq_sequence_length():
     assert sequences.get_longest_uniq_length("hwccjayhiszbmomlqkem") == 11
 
 
-def test_sum_of_strings():
-    assert sequences.get_sum_of_strings("123", "456") == "579"
-    assert sequences.get_sum_of_strings("123", "789") == "912"
-    assert sequences.get_sum_of_strings("99", "1") == "100"
 
-
-def test_sum_of_strings_big_numbers():
-    x, y, test_value = "490053634", "20846201", "510899835"
-    assert sequences.get_sum_of_strings(x, y) == test_value
-
-    x, y, test_value = "616526149", "364644330", "981170479"
-    assert sequences.get_sum_of_strings(x, y) == test_value
-
-
-def test_sum_of_strings_leading_zeros():
-    assert sequences.get_sum_of_strings("009", "000001") == "10"
-    assert sequences.get_sum_of_strings("020", "000000") == "20"
-    assert sequences.get_sum_of_strings("000", "000000") == "0"
-
-
-def test_sum_of_strings_empty():
-    assert sequences.get_sum_of_strings("123", "") == "123"
-    assert sequences.get_sum_of_strings("", "456") == "456"
-    assert sequences.get_sum_of_strings("", "") == "0"
