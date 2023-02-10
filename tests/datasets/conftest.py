f7b9beef4174abdad3d6c5901e56db6bd3349685
Serhii Horodilov <sgorodil@gmail.com>
2023-02-10 06:39:15 +0200

    Added tests for dataset filter
    Signed-off-by: Serhii Horodilov <sgorodil@gmail.com>

--- tests/datasets/conftest.py
+++ tests/datasets/conftest.py
@@ -1,4 +1,9 @@
+import csv
+from pathlib import Path
+
 import pytest
+
+FIXTURES_PATH = Path(__file__).resolve().parent.joinpath("fixtures")
 
 
 @pytest.fixture
@@ -36,3 +41,80 @@ def bricks_count():
 @pytest.fixture
 def least_bricks():
     return 1
+
+
+def read_fixture(filename):
+    with open(filename) as csv_file_handler:
+        return [row for row in csv.DictReader(csv_file_handler)]
+
+
+@pytest.fixture
+def ds_sm():
+    filename = FIXTURES_PATH.joinpath("ds_sm.csv")
+    return read_fixture(filename)
+
+
+@pytest.fixture
+def ds_sm_f1():
+    filename = FIXTURES_PATH.joinpath("ds_sm_f1.csv")
+    return read_fixture(filename), "first_name"
+
+
+@pytest.fixture
+def ds_sm_f2():
+    filename = FIXTURES_PATH.joinpath("ds_sm_f2.csv")
+    return read_fixture(filename), "gender"
+
+
+@pytest.fixture
+def ds_sm_f3():
+    filename = FIXTURES_PATH.joinpath("ds_sm_f3.csv")
+    return read_fixture(filename), "first_name", "age"
+
+
+@pytest.fixture
+def ds_sm_f4():
+    filename = FIXTURES_PATH.joinpath("ds_sm_f4.csv")
+    return read_fixture(filename), "last_name", "gender"
+
+
+@pytest.fixture
+def ds_sm_f5():
+    filename = FIXTURES_PATH.joinpath("ds_sm_f5.csv")
+    return read_fixture(filename), "last_name", "city"
+
+
+@pytest.fixture
+def ds_lg():
+    filename = FIXTURES_PATH.joinpath("ds_lg.csv")
+    return read_fixture(filename)
+
+
+@pytest.fixture
+def ds_lg_f1():
+    filename = FIXTURES_PATH.joinpath("ds_lg_f1.csv")
+    return read_fixture(filename), "first_name"
+
+
+@pytest.fixture
+def ds_lg_f2():
+    filename = FIXTURES_PATH.joinpath("ds_lg_f2.csv")
+    return read_fixture(filename), "gender"
+
+
+@pytest.fixture
+def ds_lg_f3():
+    filename = FIXTURES_PATH.joinpath("ds_lg_f3.csv")
+    return read_fixture(filename), "first_name", "age"
+
+
+@pytest.fixture
+def ds_lg_f4():
+    filename = FIXTURES_PATH.joinpath("ds_lg_f4.csv")
+    return read_fixture(filename), "last_name", "gender"
+
+
+@pytest.fixture
+def ds_lg_f5():
+    filename = FIXTURES_PATH.joinpath("ds_lg_f5.csv")
+    return read_fixture(filename), "last_name", "city"
--- tests/datasets/func_test.py
+++ tests/datasets/func_test.py
@@ -22,3 +22,53 @@ def test_original_case():
             [3, 1, 2],
             [1, 3, 1, 1]]
     assert datasets.get_least_bricks_count(wall) == 2
+
+
+def test_dataset_filter_sm1(ds_sm, ds_sm_f1):
+    filtered, *keys = ds_sm_f1
+    assert datasets.filter_by_values(ds_sm, keys) == filtered
+
+
+def test_dataset_filter_sm2(ds_sm, ds_sm_f2):
+    filtered, *keys = ds_sm_f2
+    assert datasets.filter_by_values(ds_sm, keys) == filtered
+
+
+def test_dataset_filter_sm3(ds_sm, ds_sm_f3):
+    filtered, *keys = ds_sm_f3
+    assert datasets.filter_by_values(ds_sm, keys) == filtered
+
+
+def test_dataset_filter_sm4(ds_sm, ds_sm_f4):
+    filtered, *keys = ds_sm_f4
+    assert datasets.filter_by_values(ds_sm, keys) == filtered
+
+
+def test_dataset_filter_sm5(ds_sm, ds_sm_f5):
+    filtered, *keys = ds_sm_f5
+    assert datasets.filter_by_values(ds_sm, keys) == filtered
+
+
+def test_dataset_filter_lg1(ds_lg, ds_lg_f1):
+    filtered, *keys = ds_lg_f1
+    assert datasets.filter_by_values(ds_lg, keys) == filtered
+
+
+def test_dataset_filter_lg2(ds_lg, ds_lg_f2):
+    filtered, *keys = ds_lg_f2
+    assert datasets.filter_by_values(ds_lg, keys) == filtered
+
+
+def test_dataset_filter_lg3(ds_lg, ds_lg_f3):
+    filtered, *keys = ds_lg_f3
+    assert datasets.filter_by_values(ds_lg, keys) == filtered
+
+
+def test_dataset_filter_lg4(ds_lg, ds_lg_f4):
+    filtered, *keys = ds_lg_f4
+    assert datasets.filter_by_values(ds_lg, keys) == filtered
+
+
+def test_dataset_filter_lg5(ds_lg, ds_lg_f5):
+    filtered, *keys = ds_lg_f5
+    assert datasets.filter_by_values(ds_lg, keys) == filtered
