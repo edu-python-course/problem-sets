af4a31e9315df3575686e252ac33aba3ee8f9702
Serhii Horodilov <sgorodil@gmail.com>
2023-02-20 01:07:43 +0200

    Updated "withdraw_rev"
    Added test cases and updated function boilerplate
    
    Signed-off-by: Serhii Horodilov <sgorodil@gmail.com>

--- src/atm/__init__.py
+++ src/atm/__init__.py
@@ -7,11 +7,17 @@ Withdraw
 
 .. autofunction:: withdraw
 
+Reverse withdraw
+----------------
+
+.. autofunction:: withdraw_rev
+
 """
 
 __all__ = [
     "get_total",
     "withdraw",
+    "withdraw_rev",
 ]
 
-from atm.func import get_total, withdraw
+from atm.func import get_total, withdraw, withdraw_rev
