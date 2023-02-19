4ff706c869706b94423ecfbf22d688ad26f40cc9
Serhii Horodilov <sgorodil@gmail.com>
2023-02-20 00:39:58 +0200

    Updated "atm.withdraw" tests
    Signed-off-by: Serhii Horodilov <sgorodil@gmail.com>

--- tests/atm/func_test.py
+++ tests/atm/func_test.py
@@ -20,3 +20,9 @@ def test_withdraw():
     assert atm.withdraw(2848) == [(1, 2000), (1, 500), (1, 200), (1, 100),
                                   (1, 25), (2, 10), (1, 2), (1, 1)]
     assert atm.withdraw(2000) == [(1, 2000)]
+
+
+def test_withdraw_outcome_balance():
+    assert atm.get_total(atm.withdraw(24500)) == 24500
+    assert atm.get_total(atm.withdraw(17545)) == 17545
+    assert atm.get_total(atm.withdraw(2000)) == 2000
