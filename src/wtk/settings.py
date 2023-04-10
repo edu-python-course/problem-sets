e92c33f13a358aff800f298598d1a7bc5385a405
Serhii Horodilov <sgorodil@gmail.com>
2023-04-11 00:19:37 +0300

    WKT: updated settings documentation
    Signed-off-by: Serhii Horodilov <sgorodil@gmail.com>

--- src/wtk/settings.py
+++ src/wtk/settings.py
@@ -1,10 +1,38 @@
 """
+*************
 Game settings
+*************
+
+This module contains game settings and constants.
+
+.. py:data: INITIAL_PLAYER_NAME
+
+    Initial health meter value for a player instance
+
+    :type: int
+
+.. py:data: INITIAL_ENEMY_LEVEL
+
+    Indicates the level to initialize the first enemy instance.
+
+    :type: int
+
+.. py:data: SCORE_SUCCESS_ATTACK
+
+    Set the score value to assign when an attack is successful
+
+    :type: int
+
+.. py:data: SCORE_ENEMY_DOWN
+
+    Set the score value to assign when an enemy is defeated
+
+    :type: int
 
 """
 
 # general configuration
-GAME_TITLE = "WARRIORS, ROBBERS AND WIZARDS GAME"
+GAME_TITLE = "KNIGHT, THIEFS AND WIZARDS GAME"
 
 # initial settings
 INITIAL_PLAYER_HEALTH = 5
