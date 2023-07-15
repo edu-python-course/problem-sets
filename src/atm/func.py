1b9072fc10fa00550e11f0d52ebbf2df8a451e63
Serhii Horodilov <sgorodil@gmail.com>
2023-07-15 16:55:52 +0300

    Minor fixes to code base and docstrings

--- src/atm/func.py
+++ src/atm/func.py
@@ -29,7 +29,7 @@ def withdraw(target: int,
     number of coins/bills. If no exact solution is possible, the algorithm
     returns the largest possible amount that is less than the target amount.
 
-    Usage examples:
+    Usage:
 
     >>> assert withdraw(0) == []
     >>> assert withdraw(135) == [(1, 10), (1, 25), (1, 100)]
@@ -78,7 +78,7 @@ def withdraw_rev(target: int,
     to use the smallest denominations as many times as it possible (it's
     limited with ``limit`` argument).
 
-    Usage examples:
+    Usage:
 
     >>> assert withdraw_rev(0) == []
     >>> assert withdraw_rev(14) == [(10, 1), (2, 2)]
@@ -101,7 +101,7 @@ def get_total(values: Iterable[Tuple[int, int]]) -> int:
     ``pairs`` is a list of tuples. Each ``pair`` tuple contains multiplier
     at the first position and denomination at the second one.
 
-    Usage examples:
+    Usage:
 
     >>> assert get_total([(1, 50), (1, 500), (2, 2000), (2, 10000)]) == 24500
     >>> assert get_total([(1, 500), (1, 2000), (1, 5000), (1, 10000)]) == 17545
--- src/datasets/func.py
+++ src/datasets/func.py
@@ -23,7 +23,7 @@ def swap_dict_loop(origin: Dict[Hashable, Hashable]
 
     This is the most descriptive algorithm to solve the swap task.
 
-    Example usage:
+    Usage:
 
     >>> swap_dict_loop({1: "one", 2: "two", 3: "three"})
     {"one": 1, "two": 2, "three": 3}
@@ -51,7 +51,7 @@ def swap_dict(origin: Dict[Hashable, Hashable]) -> Dict[Hashable, Hashable]:
     Works similar to ``swap_dict_loop`` function, but uses dictionary
     comprehension instead of the ``for`` loop.
 
-    Example usage:
+    Usage:
 
     >>> swap_dict({1: "one", 2: "two", 3: "three"})
     {"one": 1, "two": 2, "three": 3}
@@ -61,9 +61,9 @@ def swap_dict(origin: Dict[Hashable, Hashable]) -> Dict[Hashable, Hashable]:
     return {value: key for key, value in origin.items()}
 
 
-# define typing hints aliases
+# user defined types
 StudentData = Dict[str, Union[str, List[int]]]  # student data type alias
-StudentsList = List[StudentData]  # students data type alias
+StudentsList = List[StudentData]  # students list type alias
 
 
 def get_avg_score(student_data: StudentData) -> float:
@@ -81,7 +81,7 @@ def get_top_student(students_data: StudentsList) -> StudentData:
     :param students_data: a list of students data (names and scores)
     :type students_data: list
 
-    :return: student data entity
+    :return: student data
     :rtype: dict
 
     """
@@ -323,6 +323,7 @@ def filter_by_values(origin: List[Dict[str, Hashable]],
     return filtered_dataset
 
 
+# user defined type
 NestedIntList = Union[int, List["NestedIntList"]]
 
 
