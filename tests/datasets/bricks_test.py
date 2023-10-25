8e6ce2e965c6672d473b5f476d2da7015e1fd328
Serhii Horodilov <sgorodil@gmail.com>
2023-10-25 23:29:23 +0300

    Update bricks module tests

--- tests/datasets/bricks_test.py
+++ tests/datasets/bricks_test.py
@@ -1,17 +1,19 @@
-import datasets
+from datasets import bricks
 
 
 def test_brick_counter(bricks_wall, bricks_count):
-    assert datasets.get_bricks_count(bricks_wall) == bricks_count
+    assert bricks.get_bricks_count(bricks_wall) == bricks_count
 
 
 def test_matrix_builder(bricks_wall, position):
-    assert datasets.get_least_bricks_position(bricks_wall) == position
+    assert bricks.get_least_bricks_position(bricks_wall) == position
 
 
 def test_least_bricks(bricks_wall, small_bricks_wall):
-    assert datasets.get_least_bricks_count(bricks_wall) == 1
-    assert datasets.get_least_bricks_count(small_bricks_wall) == 3
+    assert bricks.get_least_bricks_count([[1]]) == 0
+    assert bricks.get_least_bricks_count([[1], [1]]) == 0
+    assert bricks.get_least_bricks_count(bricks_wall) == 1
+    assert bricks.get_least_bricks_count(small_bricks_wall) == 3
 
 
 def test_original_case():
@@ -21,4 +23,4 @@ def test_original_case():
             [2, 4],
             [3, 1, 2],
             [1, 3, 1, 1]]
-    assert datasets.get_least_bricks_count(wall) == 2
+    assert bricks.get_least_bricks_count(wall) == 2
