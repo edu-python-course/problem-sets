9f5f6bff36dd7476da8d3462cd9d9508b6dcc763
Rishitha-VasiReddy <rishithavasireddy27@gmail.com>
2023-11-25 01:17:16 -0500

    Update checkvowels.py
    Added docstrings

--- src/vowels/checkvowels.py
+++ src/vowels/checkvowels.py
@@ -1,9 +1,21 @@
 def is_character_vowel(char):
+    """
+    Check if a given character is a vowel.
+
+    Parameters:
+    - char (str): The character to be checked.
+
+    Returns:
+    - bool: True if the character is a vowel, False otherwise.
+    """
     vowels = "aeiouAEIOU"
     return char in vowels
 
 def main():
     user_input = input("Enter a character: ")
+    """
+    Main function to get user input and determine if the entered character is a vowel.
+    """
 
     if len(user_input) == 1 and user_input.isalpha():
         if is_character_vowel(user_input):
